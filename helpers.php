a0d19365ea4c9565474d503e5e07c9a2eef460b4
JEEMEZU <167574006+JEEMEZU@users.noreply.github.com>
2025-10-31 03:32:33 +0800

    Add helpers.php and fix email sending

--- helpers.php
+++ helpers.php
@@ -13,4 +13,14 @@ if (!function_exists('getEnv')) {
         return $default;
     }
 }
+
+if (!function_exists('generateOTP')) {
+    function generateOTP($length = 6) {
+        $otp = '';
+        for ($i = 0; $i < $length; $i++) {
+            $otp .= random_int(0, 9);
+        }
+        return $otp;
+    }
+}
 ?>
