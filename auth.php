aebc1799e9649b8787d277da1053f0fbcbf1e744
JEEMEZU <167574006+JEEMEZU@users.noreply.github.com>
2025-11-01 02:45:37 +0800

    fix

--- auth.php
+++ auth.php
@@ -1,10 +1,4 @@
-<?php require_once "controllerUserData.php";
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-?>
+<?php require_once "controllerUserData.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
