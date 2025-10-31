00025b8c5b03bc36489b53e504cb92445c782c4c
JEEMEZU <167574006+JEEMEZU@users.noreply.github.com>
2025-10-31 13:03:44 +0800

    fix: resolved Resend class conflict

--- controllerUserData.php
+++ controllerUserData.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Manila');
 
 session_start();
 require "connection.php";
-require_once "sendmail.php";
+require_once __DIR__ . "/sendmail.php";
 require_once "google-oauth-config.php";
 
 $email = "";
