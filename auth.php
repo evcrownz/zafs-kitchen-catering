936cda3410b4dd105bc74c2159f7b4d2bb3fc5db
JEEMEZU <167574006+JEEMEZU@users.noreply.github.com>
2025-11-01 02:40:22 +0800

    update auth

--- auth.php
+++ auth.php
@@ -1,15 +1,9 @@
-<?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once "controllerUserData.php";
+<?php require_once "controllerUserData.php";
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once "controllerUserData.php";
 ?>
 
 <!DOCTYPE html>
