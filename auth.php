2e70c8a65a04e5054e090be634b39e201de6510b
JEEMEZU <167574006+JEEMEZU@users.noreply.github.com>
2025-10-31 08:12:28 +0800

    Debug: Show errors

--- auth.php
+++ auth.php
@@ -1,4 +1,11 @@
-<?php require_once "controllerUserData.php"; ?>
+<?php require_once "controllerUserData.php";
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once "controllerUserData.php";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
